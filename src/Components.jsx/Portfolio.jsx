40b21e1b84a3829bdd3194c28facad933fbd40f7
omar quinones <omarquinones@omars-mbp.attlocal.net>
2020-09-23 13:40:18 -0700

    Changed fonts and sizing of pages

---
+++ src/Components.jsx/Portfolio.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+class Protfolio extends Component {
+    // state = {  }
+    render() { 
+        return (
+            <div id="portfolio">
+                <nav id="navbar">
+                    <div>
+                        <a href="#projects">Projects</a>
+                    </div>
+                    <div>
+                        <a id="profile-link" href="https://github.com/oquinone/" target="_blank">Github</a>
+                    </div>
+                </nav>
+
+                <section id="welcome-section">
+                    <div>
+                        <h1>Hey I'm Omar</h1>
+                        <small><i>Software Engineer</i></small>
+                    </div>
+                </section>
+
+                <section id="projects">
+                    <div className="project-tile">
+                        <a href="www.google.com">Calculator</a>
+                    </div>
+                    <div className="project-tile">
+                        <a href="www.google.com">Client</a>
+                    </div>
+                    <div className="project-tile">
+                        <a href="www.google.com">Test</a>
+                    </div>
+                </section>
+            </div>
+        );
+    }
+}
+ 
+export default Protfolio;
