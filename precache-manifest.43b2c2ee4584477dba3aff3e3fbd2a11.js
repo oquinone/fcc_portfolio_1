61c91efba69bc36b6fc8b4c86e06cbce096498ea
omar quinones <omarquinones@omars-mbp.attlocal.net>
2021-03-18 22:19:26 -0700

    Updates

---
+++ precache-manifest.43b2c2ee4584477dba3aff3e3fbd2a11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3cbac3da4f10e1277e99b9d9f4d0ed4",
+    "url": "/fcc_portfolio_1/index.html"
+  },
+  {
+    "revision": "49d2040bfbea92fd5b8e",
+    "url": "/fcc_portfolio_1/static/css/main.9a196e83.chunk.css"
+  },
+  {
+    "revision": "e44729af9527b5886190",
+    "url": "/fcc_portfolio_1/static/js/2.2494d31c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fcc_portfolio_1/static/js/2.2494d31c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49d2040bfbea92fd5b8e",
+    "url": "/fcc_portfolio_1/static/js/main.342a39c5.chunk.js"
+  },
+  {
+    "revision": "29ce4ac3092393274918",
+    "url": "/fcc_portfolio_1/static/js/runtime-main.f029292e.js"
+  }
+]);
